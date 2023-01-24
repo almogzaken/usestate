c6fcd5987827a4e8e90dbdfcd01b48d064ca0a59
almog zaken <almogzaken@gmail.com>
2023-01-24 10:46:54 +0200

    Added delta

--- src/Counter.js
+++ src/Counter.js
@@ -1,22 +1,32 @@
 import { useState } from "react";
 
 function Counter() {
-    const [count,setCount] = useState(0)
+    const [count,setCount] = useState(1)
+    const [delta,setdelta] = useState(1)
 
     function incr(){
         setCount(
             function(oldCount){
-                return oldCount+1;
+                return oldCount+delta
             }
         )
         console.log(count)
+    }
+    function handleDelta(e){
+        console.log(e)
+        setdelta(Number(e.target.value))
+    }
+    function reset(){
+        setCount(0)
     }
 
     return (
         <div>
             <h1> counter</h1>
+            <input type = "number" value={delta} onChange = {handleDelta}/>
             <p> Counter is at {count}</p>
-            <button onClick={incr}> Click to add 1 to Counter</button>
+            <button onClick={incr}> Click to add {delta} to Counter</button>
+            <button onClick={reset}> click to reset counter</button>
         </div>
     );
 }
